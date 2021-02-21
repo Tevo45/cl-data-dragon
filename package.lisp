004ca49ee50c1b6b27ab92569d78bb794f8ff01a
Estevan Castilho (Tevo) <estevan.cps@gmail.com>
2021-02-21 20:54:06 +0000

    Forgot to export symbols

--- package.lisp
+++ package.lisp
@@ -2,4 +2,14 @@
 
 (defpackage #:cl-data-dragon
   (:nicknames #:lol-dd)
-  (:use #:cl))
+  (:use #:cl)
+  (:export :data-dragon-client
+           :get-champ-by-name
+	   :get-champ-by-key
+           :get-champ-name-by-key
+	   
+	   :get-data
+           :version
+	   
+	   :cdn
+           :get-latest-dd-version))
